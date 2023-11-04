83e4b00bd39e03541ddb6c5ba76a61ecea48ecad
unknown <nandanasanal321@gmail.com>
2023-11-04 11:55:07 +0530

    verify

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_mobile/Screen/Authentication/Widgets/SignUp.dart';
 import 'package:ecommerce_mobile/Screen/Authentication/Widgets/Signin.dart';
 import 'package:ecommerce_mobile/Screen/HomeScreen/HomeScreenMain.dart';
+import 'package:ecommerce_mobile/temp/Verification.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return Sizer(
       builder: (context, orientation, deviceType) {
         return GetMaterialApp(
-          home: HomeScreenMain(),
+          home: VerifyScreen(),
         );
       },
     );
