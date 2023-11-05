abf9e253e37580117e63391fceac307137f6d150
Sabarinath P <sabarinath5604@gmail.com>
2023-11-05 10:12:04 +0530

    new files

--- lib/Screen/Authentication/Widgets/SignUp.dart
+++ lib/Screen/Authentication/Widgets/SignUp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -12,7 +13,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Container()],
+        children: [Container(
+
+          width: 20.w,
+          height: 30.h,
+        )],
       ),
     );
   }
